b10cfe5804ea4e1967c5945a04581eb1e64d4ee7
ShinJaehun <gunsmoke.shin@gmail.com>
2023-10-12 16:11:34 +0900

    finish triangulation_webapp

---
+++ triangulation_webapp/script.js
@@ -0,0 +1,33 @@
+function main(){
+    // console.log("it works!")
+    window.addEventListener("deviceorientation", onOrientationChange)
+
+    navigator.mediaDevices.getUserMedia({video:{facingMode:'environment'}})
+        .then(function(signal){
+            const video = document.getElementById('myVideo')
+            video.srcObject=signal
+            video.play()
+        })
+        .catch(function(err){
+            alert(err)
+        })
+}
+
+function onOrientationChange(event) {
+    let angle = event.beta - 90
+    if (angle < 0) {
+        angle = 0
+    }
+    // const distToTree = 20
+    const distToTree = document.getElementById('mySlider').value
+    document.getElementById("myLabel").innerHTML = 
+        "Distance to tree: " + distToTree + " meters"
+    // const height = Math.tan(angle)*distToTree
+    const height = Math.tan(angle*Math.PI/180)*distToTree
+
+    // console.log(angle)
+
+    document.getElementById('heightInfo').innerHTML =
+        height.toFixed(1) + "m (" + angle.toFixed(1) + "&deg;)"
+    
+}
