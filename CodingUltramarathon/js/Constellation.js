273c49c888472d9e871828a95286cee51e73acb1
ShinJaehun <gunsmoke.shin@gmail.com>
2023-11-01 14:19:09 +0900

    finish Constellation

--- CodingUltramarathon/js/Constellation.js
+++ CodingUltramarathon/js/Constellation.js
@@ -1,10 +1,18 @@
-class StarryNight {
+class Constellation {
     constructor(canvas) {
         this.canvas=canvas;
         this.ctx = canvas.getContext("2d");
         this.drawDarkBackground();
 
         this.stars=this.getRandomStars(100);
+        this.stars.push(new Star([100, 500], true))
+        this.stars.push(new Star([200, 510], true))
+        this.stars.push(new Star([300, 550], true))
+        this.stars.push(new Star([400, 600], true))
+        this.stars.push(new Star([420, 680], true))
+        this.stars.push(new Star([510, 690], true))
+        this.stars.push(new Star([550, 600], true))
+
         for(let i=0; i<this.stars.length;i++){
             this.stars[i].draw(this.ctx);
         }
@@ -21,6 +29,17 @@ class StarryNight {
             this.stars[i].update();
             this.stars[i].draw(this.ctx);
         }
+
+        for(let i=this.stars.length-6; i<this.stars.length; i++) {
+            const prevStar = this.stars[i-1];
+            const star = this.stars[i]
+            this.ctx.beginPath()
+            this.ctx.strokeStyle="white"
+            this.ctx.moveTo(...prevStar.location)
+            this.ctx.lineTo(...star.location)
+            this.ctx.stroke();
+
+        }
     }
 
     getRandomStars(N){
@@ -34,11 +53,18 @@ class StarryNight {
         }
         return arr;
     }
+    
+    drawStars(location) {
+        this.ctx.beginPath();
+        this.ctx.lineWidth=5;
+        this.ctx.fillStyle="white";
+        this.ctx.arc(location[0], location[1], 4, 0, Math.PI*2);
+        this.ctx.fill();
+    }
 
     drawDarkBackground() {
         this.ctx.beginPath();
         this.ctx.fillStyle="black";
-
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
     }
 
@@ -46,9 +72,13 @@ class StarryNight {
 }
 
 // class Star {
-//     constructor(location){
+//     constructor(location, isLarger){
 //         this.location=location;
 //         this.radius=Math.random()*2+2;// star between 2 and 4
+//         this.isLarger=isLarger
+//         if(this.isLarger){
+//             this.radius+=2
+//         }
 //     }
 //     draw(ctx){
 //         ctx.beginPath();
@@ -59,29 +89,8 @@ class StarryNight {
 //     }
 //     update(){
 //         this.radius=Math.random()*2+2;
+//         if(this.isLarger == true) {
+//             this.radius+=2;
+//         }
 //     }
 // }
-
-class Star {
-    constructor(location, isLarger){
-        this.location=location;
-        this.radius=Math.random()*2+2;// star between 2 and 4
-        this.isLarger=isLarger
-        if(this.isLarger){
-            this.radius+=2
-        }
-    }
-    draw(ctx){
-        ctx.beginPath();
-        ctx.lineWidth=5;
-        ctx.fillStyle="white";
-        ctx.arc(this.location[0], this.location[1], this.radius, 0, Math.PI*2);
-        ctx.fill();
-    }
-    update(){
-        this.radius=Math.random()*2+2;
-        if(this.isLarger == true) {
-            this.radius+=2;
-        }
-    }
-}
